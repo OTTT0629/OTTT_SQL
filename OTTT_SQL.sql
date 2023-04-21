824a31177270b0da4253863ada09928f157c5f66
NAJJUU <122864238+NAJJUU@users.noreply.github.com>
2023-04-21 16:21:32 +0900

    소율 + 주화 + 경철

--- OTTT_SQL.sql
+++ OTTT_SQL.sql
@@ -93,3 +93,35 @@ create table tb_grade(
 	gr_no 	bigint	not null primary key
 	, gr_nm 	int	null
 );
+
+drop table if exists tb_role, tb_role_to_permission, tb_permission, tb_ad;
+
+
+create table tb_role
+(
+	role_no 				smallint not null primary key
+	,field  					char(10) not null
+);
+
+create table tb_role_to_permission
+(
+	role_to_permission_no 	smallint not null primary key
+	,permission_no			smallint not null 
+	,role_no 				smallint not null
+);
+
+create table tb_permission
+(
+	permission_no 			smallint not null primary key
+	,permission_read 		smallint not null
+	,permission_write		smallint not null
+	,permission_edit		smallint not null
+	,permission_del			smallint not null
+);
+
+
+
+create table tb_ad
+(
+	ad_no					char(50) not null primary key
+);
