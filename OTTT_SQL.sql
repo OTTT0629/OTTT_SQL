21e150dc409cb491c40589889e01d325d180fad5
ddubuk228 <131105977+ddubuk228@users.noreply.github.com>
2023-04-21 17:47:49 +0900

    소율 + 주화 + 경철 + 찬희 + 가현 + 정현 + 수형

--- OTTT_SQL.sql
+++ OTTT_SQL.sql
@@ -334,5 +334,49 @@ create table tb_comment_like (
 	comment_like_no		bigint primary key
 );
 
+-- 수형
+
+--사진
+drop table if exists tb_picture;
+create table tb_picture 
+(
+	picture_no	bigint	primary key,
+	picture		text	not null
+);
+
+--내가쓴글
+drop table if exists tb_my_write;
+create table tb_my_write 
+(
+	my_write_no		bigint primary key
+);
+
+--게시글
+drop table if exists tb_article;
+create table tb_article 
+(
+	article_no			bigint			primary key,
+	article_title		varchar(255)	not null,
+	article_create_dt	timestamptz		not null,
+	article_create_user	varchar(50)		not null,
+	article_mod_dt		timestamptz
+	article_like_count	int	
+	report_cnt			int	
+);
+
+--게시글 좋아요
+drop table if exists tb_article_like;
+create table tb_article_like 
+(
+	article_like_no		bigint	primary key
+);
+
+--게시판 종류
+drop table if exists tb_article_index;
+create table tb_article_index
+(
+	article_index_no	char(1)	primary key
+);
+
 
 
